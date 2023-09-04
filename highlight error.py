857d74d7f23f8b1c9257dc73e5356de4e4368918
Mr-Dilleen <92162578+Mr-Dilleen@users.noreply.github.com>
2023-09-04 09:54:12 +0100

    test1/

---
+++ highlight error.py
@@ -0,0 +1,10 @@
+message3 = "##### ###### ######"
+message4 = "   ## ##     ##    "
+message5 = "##### ###### ######"
+message6 = "   ## ##  ##     ##"
+message7 = "##### ###### ######"
+print(message3)
+print(message4)
+print(message5)
+print(message6)
+print(message7)
